7b516fdf075afc986b5ba80aa25c02bbaa3faea6
Liao Wenchi <liaowenchi@example.com>
2026-04-08 23:28:23 +0800

    Fix Vite multipage base and script tags

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,14 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/24-percent/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        design: 'design.html',
+        impact: 'impact.html',
+        system: 'system.html'
+      }
+    }
+  }
 });
